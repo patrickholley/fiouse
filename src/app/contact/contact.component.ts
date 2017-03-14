3c83c3d63aff138ab0dc2b260eb280905fa8fa6c
Patrick Holley <patrick_holley@outlook.com>
2017-03-14 13:52:31 -0600

    working on email form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms'
 
 @Component({
   selector: 'fio-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent implements OnInit {
+export class ContactComponent {
 
   constructor() { }
-
-  ngOnInit() {
+  contact = {
+    company: 'Acme Co.',
+    name: 'Wile E. Coyote',
+    phone: '017 009 1949',
+    email: 'wecoyote@acme.com'
   }
 
+  onSubmit(form: NgForm) {
+    console.log(form.value)
+  }
 }
