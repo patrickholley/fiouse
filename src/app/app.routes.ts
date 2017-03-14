2b903ee9e030a60b94aa64c38961c2eb2c8ef246
Patrick Holley <patrick_holley@outlook.com>
2017-03-14 10:47:59 -0600

    adding routes

---
+++ src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import { Routes, RouterModule } from '@angular/router'
+import { LoginComponent } from "./login/login.component";
+
+export const APP_ROUTES: Routes = [
+    { path: 'login', component: LoginComponent }
+]
+
+export const routing = RouterModule.forRoot(APP_ROUTES)
